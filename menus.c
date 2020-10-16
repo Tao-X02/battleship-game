8729e61fe8241d6b18423171bc2d6aa1b2d9a37b
Jacqueline Ko <j34ko@uwaterloo.ca>
2020-10-17 04:38:17 +0800

    Add a menu layout. Please add stuff if I missed anything

---
+++ menus.c
@@ -0,0 +1,3 @@
+#include <LiquidCrystal.h>
+//#include led display library
+#include "menus.h"
---
+++ menus.h
@@ -0,0 +1,24 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+/*
+This will mostly just be designing the
+look of the game on the LED display
+and the printing the words on the LCD
+*/
+
+/*MENU LAYOUT*/
+
+void mainmenu();
+ void battleship();
+  //two player
+  //against AI
+   //easy
+   //medium
+    //hard
+ void settings();
+ void help();//how to play
+
+void exitbutton();
+
+#endif
